10b1a7e1af0aca3521459bc10dedae98ba2387d0
reshmacharu7-ai <reshmacharu7@gmail.com>
2026-01-17 13:12:40 +0530

    Added full Task 1 frontend + Firebase integration

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
-// Import Firebase modules from CDN
+// firebase.js
+// Firebase v10 Modular SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Your Firebase configuration
+// Firebase configuration (your project keys)
 const firebaseConfig = {
   apiKey: "AIzaSyDw9pPppOPgRx36fJ9RHacdCeenPdhcHEs",
   authDomain: "codealpha-ecommerce-4ae8f.firebaseapp.com",
   projectId: "codealpha-ecommerce-4ae8f",
-  storageBucket: "codealpha-ecommerce-4ae8f.firebasestorage.app",
+  storageBucket: "codealpha-ecommerce-4ae8f.appspot.com",
   messagingSenderId: "255596839340",
   appId: "1:255596839340:web:80bb1b9e77e5f02a477a09"
 };
@@ -16,6 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Firebase services
+// Export Auth and Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
